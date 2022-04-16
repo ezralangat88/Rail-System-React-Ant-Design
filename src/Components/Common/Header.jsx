95eeca00e2b114cec804928e15cde166a7453577
Ezra <ezra.dev98@gmail.com>
2022-04-16 20:39:40 +0300

    Features & benefits

--- src/Components/Common/Header.jsx
+++ src/Components/Common/Header.jsx
@@ -8,19 +8,17 @@ const AppHeader = () => {
       <div className="header">
         <div className="logo">
           <i className="fas fa-train"></i>
-          <a href="http://google.com">Rail System</a>
+          <a href="">Rail System</a>
         </div>
-
-       
-            <Menu mode="horizontal" defaultSelectedKeys={['Home']}>
+      
+            <Menu mode="horizontal" defaultSelectedKeys={['Home']}> 
                 <Menu.Item key="Home">Home</Menu.Item>
                 <Menu.Item key="About">About</Menu.Item>
                 <Menu.Item key="Features">Features</Menu.Item>
                 <Menu.Item key="Howitworks">How it works</Menu.Item>
                 <Menu.Item key="FAQ">FAQ</Menu.Item>
                 <Menu.Item key="Pricing">Pricing</Menu.Item>
-                <Menu.Item key="Contact">Contact</Menu.Item>
-         
+                <Menu.Item key="Contact">Contact</Menu.Item>        
             </Menu>
         
       </div>
--- src/Components/Views/Home.jsx
+++ src/Components/Views/Home.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import About from "../Home/About";
 import AppFeatures from "../Home/Features";
 import AppDescription from "../Home/Description";
-
+import Features from "../Home/Features";
 // Views
 const AppHome = () => {
   return (
     <div>
         <AppDescription/>
         <About/>
-        <AppFeatures/>
+        <Features/>
+        
     </div>
   )
 }
